2ecbb92d94cbc2d98b55edb3df313435d3c064f2
=ChiemezieUcheaga <chiemeucheaga@gmail.com>
2020-09-01 14:18:19 +0100

    Updates

---
+++ precache-manifest.7bf934b42bd3bd1f35e862eea740c593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ed2e0f0191ec511f9773c1820a728e",
+    "url": "/children-roledex/index.html"
+  },
+  {
+    "revision": "61dca6fdbbfd4244a3e5",
+    "url": "/children-roledex/static/css/main.e85f717b.chunk.css"
+  },
+  {
+    "revision": "c3ae52d375e883fbb178",
+    "url": "/children-roledex/static/js/2.88d5a660.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/children-roledex/static/js/2.88d5a660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61dca6fdbbfd4244a3e5",
+    "url": "/children-roledex/static/js/main.2e50d51d.chunk.js"
+  },
+  {
+    "revision": "21a75f5ae612f6eddc03",
+    "url": "/children-roledex/static/js/runtime-main.adc0950c.js"
+  }
+]);
